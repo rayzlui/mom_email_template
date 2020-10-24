a7ff3b271c0e88013c7efc7e008f308d8439b358
Ray <rayzlui@gmail.com>
2020-10-23 21:17:48 -0700

    Updates

---
+++ precache-manifest.cfa838a186493b940827225441968148.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "9f2ec3f79eca173345ba0a5b1615552c",
+    "url": "/mom_email_template/index.html"
+  },
+  {
+    "revision": "f1205fb3801f1cd77885",
+    "url": "/mom_email_template/static/css/main.989b98ab.chunk.css"
+  },
+  {
+    "revision": "093d41a0534b958d6d48",
+    "url": "/mom_email_template/static/js/2.39331b69.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/mom_email_template/static/js/2.39331b69.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "f1205fb3801f1cd77885",
+    "url": "/mom_email_template/static/js/main.7af878df.chunk.js"
+  },
+  {
+    "revision": "f7a0cb1bf09929bc34e3",
+    "url": "/mom_email_template/static/js/runtime-main.fe2401a5.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/mom_email_template/static/media/logo.5d5d9eef.svg"
+  }
+]);
